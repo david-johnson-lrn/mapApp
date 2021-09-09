3a0d03af65d7225dbacfe9394b34f197bfce56d6
DJ <dj@david-johnson.local>
2021-09-09 15:13:37 -0400

    added image, pinpoint class and logic to store user answer

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mapApp
+
+## Nice to Have
+Moving Element down the page to where the user clicks
+
+
+
+## Need to Have
+Clicking on image shows pin
+clicking on image records value of where they click and compares to the answer
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Assests/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>Document</title>
+
+</head>
+
+<body>
+    <div style="position: relative;">
+        <img src="./Assests/img/map.png" alt="US map" id="map">
+    </div>
+    <div id="box"></div>
+
+    <script src="/Assests/index.js"> </script>
+</body>
+
+</html>
